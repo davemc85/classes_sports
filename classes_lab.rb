cc3074d80ce3c02722dced4abd280c2d0eabc3a6
davemc85 <davemcallister@hotmail.co.uk>
2019-03-25 17:19:33 +0000

    first part of Sports Team done

--- classes_lab.rb
+++ classes_lab.rb
@@ -1,38 +1,54 @@
-class Student
-  def initialize(name, cohort)
-    @name = name
-    @cohort = cohort
+# class Student
+#   def initialize(name, cohort)
+#     @name = name
+#     @cohort = cohort
+#   end
+#
+#   def get_name
+#     return @name
+#   end
+#
+#   def get_cohort
+#     return @cohort
+#   end
+#
+#   def set_name(new_name)
+#     return @name = new_name
+#   end
+#
+#   def set_cohort(new_cohort)
+#     return @cohort = new_cohort
+#   end
+#
+#   def student_talk
+#     return "I can talk!"
+#   end
+#
+#   def say_favourite_language(language)
+#     return "I love #{language}"
+#   end
+# end
+
+
+
+
+class SportsTeam
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
   end
 
-  def get_name
-    return @name
+  def get_team_name
+    return @team_name
   end
 
-  def get_cohort
-    return @cohort
+  def get_players
+    return @players
   end
 
-  def set_name(new_name)
-    return @name = new_name
+  def get_coach
+    return @coach
   end
-
-  def set_cohort(new_cohort)
-    return @cohort = new_cohort
-  end
-
-  def student_talk
-    return "I can talk!"
-  end
-
-  def say_favourite_language(language)
-    return "I love #{language}"
-  end
-
 
 end
-
-
-
-
-# Create a method that gets the student to talk (eg. Returns "I can talk!).
-# # Create a method that takes in a students favourite programming language and returns it as part of a string (eg. student1.say_favourite_language("Ruby") -> "I love Ruby").
--- specs/classes_lab_specs.rb
+++ specs/classes_lab_specs.rb
@@ -2,41 +2,71 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../classes_lab.rb')
 
-class StudentTest < MiniTest::Test
+# class StudentTest < MiniTest::Test
+#
+#   def test_name_getter
+#     student = Student.new("David", "G12")
+#     assert_equal("David", student.get_name)
+#   end
+#   #
+#   def test_name_setter
+#     student = Student.new("David", "G12")
+#     student.set_name("John")
+#     assert_equal("John", student.get_name)
+#   end
+#
+#   def test_cohort
+#     student = Student.new("David", "G12")
+#     assert_equal("G12", student.get_cohort)
+#   end
+#
+#
+#   def test_set_cohort
+#     student = Student.new("David", "G12")
+#     student.set_cohort("G11")
+#     assert_equal("G11", student.get_cohort)
+#   end
+#
+#   def test_student_talk
+#     student = Student.new("David", "G12")
+#     assert_equal("I can talk!", student.student_talk)
+#   end
+#
+#   def test_fav_programming_language
+#     student = Student.new("David", "G12")
+#     student.say_favourite_language("Ruby")
+#     assert_equal("I love Ruby", student.say_favourite_language("Ruby"))
+#   end
+#
+#
+# end
 
-  def test_name_getter
-    student = Student.new("David", "G12")
-    assert_equal("David", student.get_name)
+
+class SportsTeamTest < MiniTest::Test
+
+  def test_get_team_name
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard")
+    assert_equal("Rangers", sports_team.get_team_name)
   end
-  #
-  def test_name_setter
-    student = Student.new("David", "G12")
-    student.set_name("John")
-    assert_equal("John", student.get_name)
+
+  def test_get_players
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard")
+    assert_equal(["Morelos", "Defoe"], sports_team.get_players)
   end
 
-  def test_cohort
-    student = Student.new("David", "G12")
-    assert_equal("G12", student.get_cohort)
+  def test_get_coach
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard")
+    assert_equal("Gerrard", sports_team.get_coach)
   end
-
-
-  def test_set_cohort
-    student = Student.new("David", "G12")
-    student.set_cohort("G11")
-    assert_equal("G11", student.get_cohort)
-  end
-
-  def test_student_talk
-    student = Student.new("David", "G12")
-    assert_equal("I can talk!", student.student_talk)
-  end
-
-  def test_fav_programming_language
-    student = Student.new("David", "G12")
-    student.say_favourite_language("Ruby")
-    assert_equal("I love Ruby", student.say_favourite_language("Ruby"))
-  end
-
-
 end
+
+
+# For each property in the class make a getter method than can return them.
+# Create a setter method to allow the coach's name to be updated.
+# Refactor the class to use attr_reader or attr_accessor instead of your own getter and
+#
+# setter methods.
+# Create a method that adds a new player to the players array.
+# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+# Add a points property into your class that starts at 0.
+# Create a method that takes in whether the team has won or lost and updates the points property for a win.
