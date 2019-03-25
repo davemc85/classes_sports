6af855e1bad2d3daea75fda0c0a3637f637d913b
davemc85 <davemcallister@hotmail.co.uk>
2019-03-25 20:24:41 +0000

    homework complete except extensions

--- classes_lab.rb
+++ classes_lab.rb
@@ -1,54 +1,79 @@
-# class Student
-#   def initialize(name, cohort)
-#     @name = name
-#     @cohort = cohort
-#   end
-#
-#   def get_name
-#     return @name
-#   end
-#
-#   def get_cohort
-#     return @cohort
-#   end
-#
-#   def set_name(new_name)
-#     return @name = new_name
-#   end
-#
-#   def set_cohort(new_cohort)
-#     return @cohort = new_cohort
-#   end
-#
-#   def student_talk
-#     return "I can talk!"
-#   end
-#
-#   def say_favourite_language(language)
-#     return "I love #{language}"
-#   end
-# end
+class Student
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def get_name
+    return @name
+  end
+
+  def get_cohort
+    return @cohort
+  end
+
+  def set_name(new_name)
+    return @name = new_name
+  end
+
+  def set_cohort(new_cohort)
+    return @cohort = new_cohort
+  end
+
+  def student_talk
+    return "I can talk!"
+  end
+
+  def say_favourite_language(language)
+    return "I love #{language}"
+  end
+end
 
 
 
 
 class SportsTeam
-  def initialize(team_name, players, coach)
+
+  attr_accessor :team_name, :players, :coach, :points
+
+
+  def initialize(team_name, players, coach, points)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = points
   end
 
-  def get_team_name
-    return @team_name
+  def add_player(player)
+    @players << player
+    return sports_team[:players].length
   end
 
-  def get_players
-    return @players
+  def find_player(search_player)
+    for name in @players
+      if name == search_player
+        return true
+      end
+    end
   end
 
-  def get_coach
-    return @coach
+  def match_result(result)
+    if result == "Win"
+      @points += 3
+    elsif result == "Draw"
+      @points += 1
+    else
+      @points += 0
+    end
   end
+end
+
+
+
+
+class Library
+  def initialize
+    @title
+    @rental_details
 
 end
--- specs/classes_lab_specs.rb
+++ specs/classes_lab_specs.rb
@@ -2,71 +2,96 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../classes_lab.rb')
 
-# class StudentTest < MiniTest::Test
-#
-#   def test_name_getter
-#     student = Student.new("David", "G12")
-#     assert_equal("David", student.get_name)
-#   end
-#   #
-#   def test_name_setter
-#     student = Student.new("David", "G12")
-#     student.set_name("John")
-#     assert_equal("John", student.get_name)
-#   end
-#
-#   def test_cohort
-#     student = Student.new("David", "G12")
-#     assert_equal("G12", student.get_cohort)
-#   end
-#
-#
-#   def test_set_cohort
-#     student = Student.new("David", "G12")
-#     student.set_cohort("G11")
-#     assert_equal("G11", student.get_cohort)
-#   end
-#
-#   def test_student_talk
-#     student = Student.new("David", "G12")
-#     assert_equal("I can talk!", student.student_talk)
-#   end
-#
-#   def test_fav_programming_language
-#     student = Student.new("David", "G12")
-#     student.say_favourite_language("Ruby")
-#     assert_equal("I love Ruby", student.say_favourite_language("Ruby"))
-#   end
-#
-#
-# end
+class StudentTest < MiniTest::Test
+
+  def test_name_getter
+    student = Student.new("David", "G12")
+    assert_equal("David", student.get_name)
+  end
+  #
+  def test_name_setter
+    student = Student.new("David", "G12")
+    student.set_name("John")
+    assert_equal("John", student.get_name)
+  end
+
+  def test_cohort
+    student = Student.new("David", "G12")
+    assert_equal("G12", student.get_cohort)
+  end
+
+
+  def test_set_cohort
+    student = Student.new("David", "G12")
+    student.set_cohort("G11")
+    assert_equal("G11", student.get_cohort)
+  end
+
+  def test_student_talk
+    student = Student.new("David", "G12")
+    assert_equal("I can talk!", student.student_talk)
+  end
+
+  def test_fav_programming_language
+    student = Student.new("David", "G12")
+    student.say_favourite_language("Ruby")
+    assert_equal("I love Ruby", student.say_favourite_language("Ruby"))
+  end
+
+
+end
 
 
 class SportsTeamTest < MiniTest::Test
 
   def test_get_team_name
-    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard")
-    assert_equal("Rangers", sports_team.get_team_name)
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    assert_equal("Rangers", sports_team.team_name)
   end
 
   def test_get_players
-    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard")
-    assert_equal(["Morelos", "Defoe"], sports_team.get_players)
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    assert_equal(["Morelos", "Defoe"], sports_team.players)
   end
 
   def test_get_coach
-    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard")
-    assert_equal("Gerrard", sports_team.get_coach)
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    assert_equal("Gerrard", sports_team.coach)
   end
+
+
+  def test_coach_setter
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    sports_team.coach = "Smith"
+    assert_equal("Smith", sports_team.coach)
+  end
+
+  def test_add_player
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    sports_team.players.push("Davis")
+    assert_equal(3, sports_team.players.length)
+  end
+
+  def test_find_player
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    player = sports_team.find_player("Defoe")
+    assert_equal(true, player)
+  end
+
+  def test_match_result
+    sports_team = SportsTeam.new("Rangers", ["Morelos", "Defoe"], "Gerrard", 0)
+    result = sports_team.match_result("Win")
+    assert_equal(3, result)
+  end
+
 end
 
 
-# For each property in the class make a getter method than can return them.
-# Create a setter method to allow the coach's name to be updated.
-# Refactor the class to use attr_reader or attr_accessor instead of your own getter and
-#
-# setter methods.
-# Create a method that adds a new player to the players array.
-# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
-# Add a points property into your class that starts at 0.
-# Create a method that takes in whether the team has won or lost and updates the points property for a win.
+
+class LibraryTest < MiniTest::Test
+
+
+
+
+
+end
