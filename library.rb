3e29b2531c59f21b4107715569be71244b5c8f21
davemc85 <davemcallister@hotmail.co.uk>
2019-03-26 11:09:50 +0000

    part c started

--- library.rb
+++ library.rb
@@ -1,6 +1,30 @@
 class Library
-  def initialize(title, person, due_date)
-    @title = title
-    @rental_details = person, due_date
+  def initialize(books)
+    @books = books
+
+  end
+
+
+  def get_books()
+    return @books
+  end
+
+  def find_by_title(title)
+    for book in @books
+      if book[:title] == title
+        return book
+      end
+    end
+    return nil
+  end
+
+  def find_rental_details(title)
+    book = find_by_title(title)
+    return book[:rental_details] if book != nil
+    return nil
+  end
+
+
+
 
 end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -4,10 +4,41 @@ require_relative('../library.rb')
 
 
 class LibraryTest < MiniTest::Test
-  def test_book_name
-    book = Library.new("Fever Pitch", RentalDetails.new("David", "01/08/2018"))
 
+  def setup()
+    book = {
+      title: "Fever Pitch",
+      rental_details: {
+        student_name: "David",
+        date: "01/08/2018"
+      }
+    }
 
+      @library = Library.new([book])
+  end
+
+  def test_library_has_book()
+    assert_equal([{
+    title: "Fever Pitch",
+    rental_details: {
+      student_name: "David",
+      date: "01/08/2018"
+    }
+    }], @library.get_books())
+  end
+
+  def test_can_get_book()
+    book = @library.find_by_title("Fever Pitch")
+    assert_equal("Fever Pitch", book[:title])
+  end
+
+  def test_can_get_rental_details()
+    rental_details = @library.find_rental_details("Fever Pitch")
+    assert_equal({
+      student_name: "David",
+      date: "01/08/2018"
+    }, rental_details)
+  end
 
 
 end
